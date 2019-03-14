8cf189f35ebae4b21600abcd9389abd564ef8d17
DeboraSR <48536645+DeboraSR@users.noreply.github.com>
2019-03-13 23:49:38 -0300

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,16 @@
+from  lib.scrapy_table import Scrapy_Table
+
+url="https://pt.wikipedia.org/wiki/C%C3%A2mara_Municipal_de_S%C3%A3o_Paulo"
+
+site_connect = Scrapy_Table(url)
+
+tables = site_connect.get_tables(5)
+  
+partidos = set() 
+
+for linha in tables[1:]:
+
+    partidos.add(linha[1])
+
+for partido in partidos:
+    print(partido)
